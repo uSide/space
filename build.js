24adaa8fc7be911223c8c1ab2e9345682c15d450
Vladimir Kleshko <udp.tcp@ya.ru>
2015-03-10 15:04:01 +0200

    build fix

--- build.js
+++ build.js
@@ -1,49 +1,65 @@
 var dir = 'game/entities/',
-  files = ['entity', 'star', 'item', 'engine', 'shell', 'ship'],
-  renderers = ['ship', 'star'],
-  cp = require('child_process'),
-  fs = require('fs'),
-  _ = require('lodash');
+    files = ['entity', 'star', 'item', 'engine', 'shell', 'ship'],
+    renderers = ['ship', 'star'],
+    cp = require('child_process'),
+    fs = require('fs'),
+    _ = require('lodash'),
+    path = require('path'),
+    async = require('async');
 
-var contents = '';
+var contents = [];
 
-files.forEach(function(file) {
-  var output = cp.spawnSync('c:\\Users\\Flopix\\AppData\\Roaming\\npm\\babel.cmd', [
-    '--playground',
-    '--experimental',
-    dir + file + '.js'
-  ]).stdout.toString();
+var translate = function(file, callback){
+    var output = '';
 
-  contents += output;
+    var proc = cp.fork(path.join(".", "node_modules", "babel", "bin", "babel", "index.js"), [
+        '--playground',
+        '--experimental',
+        dir + file + '.js'
+    ], {
+        silent: true
+    });
+
+    proc.stdout.on('data', function(data) {
+        output += data.toString();
+    });
+
+    proc.on('exit', function(){
+        callback(null, output);
+    });
+};
+
+async.map(files, translate, function(err, contents){
+    contents = _.values(contents).join('');
+
+    fs.writeFileSync('./game/entities.js', contents);
+
+    contents += 'module.exports = {';
+
+    contents += 'entities:entities,';
+
+    files.forEach(function(file) {
+        contents += _.capitalize(file) + ':' + _.capitalize(file) + ',';
+    });
+
+    contents += '}';
+
+    fs.writeFileSync('./logic/entities.js', contents);
+
+    // clear
+    contents = 'window.renderEntity = function(entity){\n';
+
+    renderers.forEach(function(file) {
+        contents += 'if (entity instanceof ' + _.capitalize(file) + ') {\n';
+
+        contents += fs.readFileSync(dir + file + '.render.js', {
+            encoding: 'utf8'
+        });
+
+        contents += '}\n';
+    });
+
+    contents += '};\n';
+
+    fs.writeFileSync('./game/render.js', contents);
 });
-
-fs.writeFileSync('./game/entities.js', contents);
-
-contents += 'module.exports = {';
-
-contents += 'entities:entities,';
-
-files.forEach(function(file) {
-  contents += _.capitalize(file) + ':' + _.capitalize(file) + ',';
-});
-
-contents += '}';
-
-fs.writeFileSync('./logic/entities.js', contents);
-
-// clear
-contents = 'window.renderEntity = function(entity){\n';
-
-renderers.forEach(function(file) {
-  contents += 'if (entity instanceof ' + _.capitalize(file) + ') {\n';
-
-  contents += fs.readFileSync(dir + file + '.render.js', {
-    encoding: 'utf8'
-  });
-
-  contents += '}\n';
-});
-
-contents += '};\n';
-
-fs.writeFileSync('./game/render.js', contents);
